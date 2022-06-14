d5abf3742792fb97e630df6e819c383a50a1babc
Thong Doan <dmthong181096@gmail.com>
2022-06-14 22:02:18 +0700

    a

--- ReactApp/App.js
+++ ReactApp/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Home from './Source/HomeScreen';
 export default function App() {
+  const name = "Thong"
   return (
+   
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style= {styles.textStyle1}>This is Style 1 with font size 45</Text>
+      <Text style= {styles.textStyle2}>Hello {name},this is Style 2 with font size 30</Text>
+      <Text> {Home()}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +16,16 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textStyle1: {
+    fontSize: 35,
+    backgroundColor: 'red'
+  },
+  textStyle2: {
+    fontSize: 10
+  }
 });
