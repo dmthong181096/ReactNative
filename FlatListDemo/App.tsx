a9ec39efeb09fbb5f4e1b15290c2936d7caf7b3d
Thong Doan <dmthong181096@gmail.com>
2022-06-16 22:22:00 +0700

    a

--- FlatListDemo/App.tsx
+++ FlatListDemo/App.tsx
@@ -1,16 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';z
+import { StyleSheet, Text, View ,Button, TouchableOpacity} from 'react-native';
 import FlatListDemo from './Source/FlatListDemo';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
       {/* <Text>Open up App.tsx to start working on your app!</Text> */}
-      <StatusBar  />
-      {FlatListDemo()}
+      {/* <StatusBar /> */}
+      {/* {FlatListDemo()} */}
+      <Button title='"HomeScreen' onPress={() => {console.log("Hello! ")}}></Button>
+
+      <Button title='"FlatList'></Button> 
+      <TouchableOpacity ><Text>dsds</Text></TouchableOpacity>
     </View>
   );
 }
+// const navigatior = createStackNavigator( {
+
+// })
+ let SayHello = (name:String)=> {
+  console.log("Hello! ", name)
+}
+
+
 
 const styles = StyleSheet.create({
   container: {
--- FlatListDemo/Source/FlatListDemo.tsx
+++ FlatListDemo/Source/FlatListDemo.tsx
@@ -1,37 +1,40 @@
 // import React from 'react';
-import { Text, View,FlatList ,StyleSheet} from 'react-native';
+import { Text, View, FlatList, StyleSheet, SafeAreaView } from 'react-native';
 
 const FlatListDemo = () => {
   const friends = [
-    {name: "Friend #1" , age: 1},
-    {name: "Friend #2" , age: 2},
-    {name: "Friend #3" , age: 3},
-    {name: "Friend #4" , age: 4},
-    {name: "Friend #5" , age: 5},
-    {name: "Friend #6" , age: 6},
-    {name: "Friend #7" , age: 7},
-    {name: "Friend #8" , age: 8},
-    {name: "Friend #9" , age: 9},
+    { name: "Friend #1", age: 1 },
+    { name: "Friend #2", age: 2 },
+    { name: "Friend #3", age: 3 },
+    { name: "Friend #4", age: 4 },
+    { name: "Friend #5", age: 5 },
+    { name: "Friend #6", age: 6 },
+    { name: "Friend #7", age: 7 },
+    { name: "Friend #8", age: 8 },
+    { name: "Friend #9", age: 9 },
 
   ]
   return (
-   <FlatList 
-            showsVerticalScrollIndicator = { false}
-            data={friends}
-            renderItem = {({item}) => {
-              return <Text style= {styles.textStyle1}>Name: {item.name} ------ Age: {item.age}</Text>
-            }}
-   
-   >
-   </FlatList>
+    <SafeAreaView>
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={friends}
+        renderItem={({ item }) => {
+          return <Text style={styles.textStyle1}>{item.name} - Age: {item.age}</Text>
+        }}
+
+      >
+      </FlatList>
+    </SafeAreaView>
+
   );
 }
-const  styles = StyleSheet.create ({
-        textStyle1: {
-          fontSize: 30,
-          marginVertical: 40,
-          alignContent: "center"
-        }
+const styles = StyleSheet.create({
+  textStyle1: {
+    fontSize: 30,
+    marginVertical: 40,
+    alignContent: "center"
+  }
 })
 
 export default FlatListDemo;
